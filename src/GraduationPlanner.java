6ee01854b1ae9118f95a3deefddc7ed152d8e6d6
Katrina Brinkmann <kmbrinkm@outlook.com>
2026-04-20 11:32:28 -0500

    Created main class

---
+++ src/GraduationPlanner.java
@@ -0,0 +1,21 @@
+import edu.macalester.graphics.CanvasWindow;
+import java.awt.Color;
+
+public class GraduationPlanner {
+    private static final int CANVAS_WIDTH = 1600;
+    private static final int CANVAS_HEIGHT = 800;
+    private static final Color CANVAS_COLOR = Color.LIGHT_GRAY;
+
+    private CanvasWindow canvas;
+
+    public static void main(String[] args) {
+        new GraduationPlanner();
+    }
+
+    public GraduationPlanner(){
+        canvas = new CanvasWindow("Graduation Planner", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas.setBackground(CANVAS_COLOR);
+    }
+
+
+}
