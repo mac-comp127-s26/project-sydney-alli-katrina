84fb414a30ca9e62a353d75376bf402a87bf8807
Allijohnson <ajohns40@macalester.edu>
2026-05-04 12:09:12 -0500

    ReadMe File
    Added readme requirements

--- src/SemesterManager.java
+++ src/SemesterManager.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.FontStyle;
-import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.GraphicsText;
 import edu.macalester.graphics.Rectangle;
 
